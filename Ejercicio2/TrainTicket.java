bd43ab39b5574d479ad5d0c86fba78e546828bad
Alvaro Peinado De Arcos <alvaro.s404977@cesurformacion.com>
2024-11-28 08:43:16 +0100

    a

---
+++ Ejercicio2/TrainTicket.java
@@ -0,0 +1,27 @@
+package Ejercicio2;
+
+import Ejercicio1.Vehiculo;
+
+public class TrainTicket extends Ticket {
+    public TrainTicket(String ticketNumber, double basePrice) {
+        super(ticketNumber, basePrice);
+    }
+
+    @Override
+    public double calculatePrice() {
+        return getBasePrice() + 10; // ESTE ES EL IMPUESTO DE 10 EUROS
+    }
+
+    public double calculatePrice(boolean isPeakTime) {
+        double price = calculatePrice();
+        if (isPeakTime) {
+            price += 5; //ESTE ES EL IMPUESTO DE 5 EUROS
+        }
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "TrainTicket{" + super.toString() + "}";
+    }
+}
